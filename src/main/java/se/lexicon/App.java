f8f7989d45561176db7f44a5cb3ffd61ed38ab5a
Emma Åstrand <aastrand.emma@gmail.com>
2024-03-21 12:32:46 +0100

    Feat: Part 1 (Book Lender)

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,26 +1,42 @@
 package se.lexicon;
 
+import se.lexicon.model.Book;
+import se.lexicon.model.Person;
 
 public class App {
     public static void main(String[] args) {
-        // todo: needs completion
-
         // Create a book instance
+        Book book = new Book("Jane Doe in Paris", "John Doe");
         // Display book information
+        String dpBook = book.getBookInformation();
+        System.out.println("dpBook = " + dpBook);
 
         // Create a person instance
+        Person person = new Person("Emma", "Åstrand");
         // Display person information
+        String dpPerson = person.getPersonInformation();
+        System.out.println("dpPerson = " + dpPerson);
+        System.out.println();
 
         // Loan a book to the person
+        person.loanBook(book);
 
         // Display person information after borrowing a book
+        String dpPersonLoan = person.getPersonInformation();
+        System.out.println("dpPersonLoan = " + dpPersonLoan);
         // Display book information after borrowing a book
+        String dpBookLoan = book.getBookInformation();
+        System.out.println("dpBookLoan = " + dpBookLoan);
+        System.out.println();
 
         // Return the borrowed book
+        person.returnBook(book);
 
         // Display person information after returning the book
+        String dpPersonReturn = person.getPersonInformation();
+        System.out.println("dpPersonReturn = " + dpPersonReturn);
         // Display book information after borrowing a book
-
+        String dpBookReturn = book.getBookInformation();
+        System.out.println("dpBookReturn = " + dpBookReturn);
     }
-
 }
--- src/main/java/se/lexicon/model/Book.java
+++ src/main/java/se/lexicon/model/Book.java
@@ -1,5 +1,64 @@
 package se.lexicon.model;
 
+import java.util.UUID;
+
 public class Book {
-    // todo: needs completion
+    private String id;
+    private String title;
+    private String author;
+    private boolean available;
+    private Person borrower;
+
+    public Book(String title, String author) {
+        setId();
+        setTitle(title);
+        setAuthor(author);
+        this.available = true;
+    }
+
+    public Book(String title, String author, Person borrower) {
+        this(title, author);
+        setBorrower(borrower);
+    }
+
+    private void setId() {
+        this.id = UUID.randomUUID().toString().substring(0,8).toUpperCase();
+    }
+
+    public void setTitle(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            throw new IllegalArgumentException("Title can't be NULL or empty.");
+        } else {
+            this.title = title;
+        }
+    }
+
+    public void setAuthor(String author) {
+        if (author == null || author.trim().isEmpty()) {
+            throw new IllegalArgumentException("Author can't be NULL or empty.");
+        } else {
+            this.author = author;
+        }
+    }
+
+    public void setBorrower(Person borrower) {
+        if (borrower != null) this.available = false;
+        this.borrower = borrower;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public String getBookInformation() {
+        return "id: " + id + " title: " + title + " author: " + author + ", available: " + available;
+    }
 }
